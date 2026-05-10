0f6aa3db4082f5ba60fd471817b74fdd3e7e2e44
shahzaad4 <shahzaadalhaq@gmail.com>
2026-05-10 16:41:18 +0300

    Fixed the add walk function, running now

--- NZWalks.API/Models/DTO/AddWalkRequestDTO.cs
+++ NZWalks.API/Models/DTO/AddWalkRequestDTO.cs
@@ -7,7 +7,7 @@
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
 
-        public Guid DifficultId { get; set; }
+        public Guid DifficultyId { get; set; }
         public Guid RegionId { get; set; }
     }
 }
